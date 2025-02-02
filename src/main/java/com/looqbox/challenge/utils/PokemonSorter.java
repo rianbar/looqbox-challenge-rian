41bb9b4ccdca139fbc7387a5f3dc17eb005b64fb
rianbar <rianbarroso596@gmail.com>
2025-02-02 19:51:34 -0300

    docs: insert sort algorithms explanations

--- src/main/java/com/looqbox/challenge/utils/PokemonSorter.java
+++ src/main/java/com/looqbox/challenge/utils/PokemonSorter.java
@@ -10,6 +10,8 @@ import com.looqbox.challenge.constant.SortType;
 @Component
 public class PokemonSorter {
 
+    // based on the sort type, this method calls the bubbleSortList method to sort the pokemons
+    // with the correct comparator
     public List<String> sortPokemons(List<String> pokemons, SortType sort) {
         switch (sort) {
             case ALPHABETICAL:
@@ -21,6 +23,9 @@ public class PokemonSorter {
         }
     }
     
+    // this method uses the bubble sort algorithm to sort the pokemons, it receives a Bipredicate to compare the pokemons-
+    // based on the sort type
+    // BIG 0 notation: O(n^2) (medium case), the method receives a unordered list of pokemons and sorts them
     private List<String> bubbleSortList(List<String> pokemons, BiPredicate<String, String> compare) {
         int size = pokemons.size();
         for (int i = 0; i < size; i++) {
@@ -35,6 +40,9 @@ public class PokemonSorter {
         return pokemons;
     }
 
+    // here is the implementation of the alphabetical comparation, it compares the pokemons lexicographically-
+    // if the first pokemon is lexicographically smaller than the second, it returns true, otherwise it returns false-
+    // if the first pokemon is lexicographically equal to the second, this means that both strings are equal, so sort by length
     private boolean alphabeticalSort(String a, String b) {
         int min = Math.min(a.length(), b.length());
         for (int i = 0; i < min; i++) {
@@ -45,6 +53,7 @@ public class PokemonSorter {
         return a.length() > b.length();
     }
 
+    // here is a simple implementation of the lenght comparation, it compares the pokemons based on their lenghts
     private boolean lenghtSort(String a, String b) {
         return a.length() < b.length();
     }   

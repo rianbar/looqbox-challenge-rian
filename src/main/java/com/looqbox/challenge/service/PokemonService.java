6d778641f360d3b893bbeaf3a9f49093c22fa4e9
rianbar <rianbarroso596@gmail.com>
2025-02-04 14:45:01 -0300

    feat: cache manager and startup saver

--- src/main/java/com/looqbox/challenge/service/PokemonService.java
+++ src/main/java/com/looqbox/challenge/service/PokemonService.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.looqbox.challenge.constant.SortType;
-import com.looqbox.challenge.model.response.ApiResponse;
 import com.looqbox.challenge.model.response.HighlightResponse;
+import com.looqbox.challenge.model.response.PokemonName;
 import com.looqbox.challenge.model.response.PokemonResponse;
+import com.looqbox.challenge.service.cache.InMemoryCacheManager;
 import com.looqbox.challenge.utils.PokemonFilter;
 import com.looqbox.challenge.utils.PokemonFormatter;
 import com.looqbox.challenge.utils.PokemonSorter;
@@ -16,35 +17,31 @@ import com.looqbox.challenge.utils.PokemonSorter;
 @Service
 public class PokemonService {
     
-    private final PokeApiService pokeApi;
+    private final InMemoryCacheManager cache;
     private final PokemonSorter sorter;
     private final PokemonFilter filter;
     private final PokemonFormatter formatter;
 
-    public PokemonService(PokeApiService pokeApiService, PokemonSorter sorter, PokemonFilter filter, PokemonFormatter formatter) {
-        this.pokeApi = pokeApiService;
+    public PokemonService(InMemoryCacheManager cache, PokemonSorter sorter, PokemonFilter filter, PokemonFormatter formatter) {
+        this.cache = cache;
         this.sorter = sorter;
         this.filter = filter;
         this.formatter = formatter; 
     }
 
     public PokemonResponse<String> getPokemonService(String query, SortType sort) {
-        return sortPokemons(this.fetchPokemons().get(), query, sort);
+        return sortPokemons(cache.getAll(), query, sort);
     }
 
     public PokemonResponse<HighlightResponse> getPokemonHighlightsService(String query, SortType sort) {
-        PokemonResponse<String> sortedPokemons = sortPokemons(this.fetchPokemons().get(), query, sort);
+        PokemonResponse<String> sortedPokemons = sortPokemons(cache.getAll(), query, sort);
         return new PokemonResponse<>(formatter.toHighlightResponse(sortedPokemons.getResult(), query));
     }
 
-    private Optional<ApiResponse> fetchPokemons() {
-        return Optional.ofNullable(pokeApi.retrieveAllPokemons());
-    }
-
-    private PokemonResponse<String> sortPokemons(ApiResponse response, String query, SortType sort) {
+    private PokemonResponse<String> sortPokemons(List<PokemonName> response, String query, SortType sort) {
         List<String> result = Optional.ofNullable(query)
-            .map(q -> filter.filterPokemons(response.getResults(), q))
-            .orElse(formatter.toStringList(response.getResults()));
+            .map(q -> filter.filterPokemons(response, q))
+            .orElse(formatter.toStringList(response));
             
         return new PokemonResponse<>(sorter.sortPokemons(result, sort));
     }
